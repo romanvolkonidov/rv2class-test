9b6587c8dafe37d49c78b9bb0d3521d6dc8adb41
romanvolkonidov <romanvolkonidov@gmail.com>
2025-10-07 14:42:36 +0300

    fixed imports

--- app/student/[id]/page.tsx
+++ app/student/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { notFound } from "next/navigation";
-import StudentWelcome from "./student-welcome";
+import StudentWelcome from "./student-welcome.js";
 import { db } from "@/lib/firebase";
 import { doc, getDoc } from "firebase/firestore";
 
